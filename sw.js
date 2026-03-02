ada1236d3bbb2b0aae33f63569cee89868ab3416
sandada035-blip <sandada035@gmail.com>
2026-03-02 13:18:41 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "budget-app-v2";
+const CACHE_NAME = "budget-app-v3";
 const urlsToCache = [
   "./",
   "./index.html",
@@ -8,25 +8,20 @@ const urlsToCache = [
   "./logo.png"
 ];
 
-// Install
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Activate (clean old cache)
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : Promise.resolve()))
-      )
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : Promise.resolve())))
     )
   );
 });
 
-// Fetch
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((cached) => cached || fetch(event.request))
